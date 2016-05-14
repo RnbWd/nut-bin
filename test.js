04d792be955dda8f370b09787e179196ecd6e863
David Wisner <dwisner6@gmail.com>
2016-05-14 11:58:55 -0700

    no test

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 'use strict';
 var test = require('ava');
 var binCheck = require('bin-check');
-var vault = require('./');
+var nut = require('./cli.js');
 
 test('return path to binary and verify that it is working', function (t) {
   t.plan(2);
 
-  binCheck(vault, ['--version'], function (err, stdout) {
+  binCheck(nut, ['--version'], function (err, stdout) {
     t.assert(!err, err);
     t.assert(stdout);
   });
